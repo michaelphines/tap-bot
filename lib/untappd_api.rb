4763f4164e4eef3383a67cb1101e3ad03b4f191b
Michael Hines <mhines@groupon.com>
2015-02-07 14:30:04 -0600

    Shorten description if it's too long

--- lib/untappd_api.rb
+++ lib/untappd_api.rb
@@ -4,6 +4,8 @@ require 'app_config'
 class UntappdApi < Api
   base_uri AppConfig.untappd.base
 
+  MAX_LENGTH = 575
+
   def initialize(options = {})
     self.defaults = { :apikey => AppConfig.untappd.apikey }.merge(options)
   end
@@ -19,13 +21,22 @@ class UntappdApi < Api
     get(AppConfig.untappd.search, :q => query).results
   end
 
+  def minimize_description(description)
+    collapsed_description = description.gsub(/\s+/, " ")
+    if collapsed_description.length > MAX_LENGTH
+      collapsed_description[0..MAX_LENGTH].rstrip + "..."
+    else
+      collapsed_description
+    end
+  end
+
   def show(id)
     results = get(AppConfig.untappd.show, :kimpath2 => id).results
     details = results.details.first
     photos = results.photos.map { |p| HTTParty.get(p.photo.src).parsed_response }
 
     details.merge(
-      :description => details.description && details.description.gsub(/\s+/, " "),
+      :description => details.description && minimize_description(details.description),
       :photos => photos
     )
   end
